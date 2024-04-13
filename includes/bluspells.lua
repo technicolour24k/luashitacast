31c8c86b6b8a9e10c0def4bdecc8eb9ee9029450
technicolour24k <hitetsu@hotmail.com>
2024-04-13 01:11:39 +0100

    Added BLU spells

--- includes/bluspells.lua
+++ includes/bluspells.lua
@@ -38,7 +38,594 @@ bluspells = {
     ['yawn'] = {
         spell="Yawn",
         target="t"
-    }
+    },
+    ["venomshell"] = {
+        spell = "Venom Shell",
+        target = "t"
+    },
+    ["maelstrom"] = {
+        spell = "Maelstrom",
+        target = "t"
+    },
+    ["metallicbody"] = {
+        spell = "Metallic Body",
+        target = "me"
+    },
+    ["screwdriver"] = {
+        spell = "Screwdriver",
+        target = "t"
+    },
+    ["mpdrainkiss"] = {
+        spell = "MP Drainkiss",
+        target = "t"
+    },
+    ["deathray"] = {
+        spell = "Death Ray",
+        target = "t"
+    },
+    ["sandspin"] = {
+        spell = "Sandspin",
+        target = "t"
+    },
+    ["smiteofrage"] = {
+        spell = "Smite of Rage",
+        target = "t"
+    },
+    ["bludgeon"] = {
+        spell = "Bludgeon",
+        target = "t"
+    },
+    ["refueling"] = {
+        spell = "Refueling",
+        target = "me"
+    },
+    ["icebreak"] = {
+        spell = "Ice Break",
+        target = "t"
+    },
+    ["blitzstrahl"] = {
+        spell = "Blitzstrahl",
+        target = "t"
+    },
+    ["selfdestruct"] = {
+        spell = "Self-Destruct",
+        target = "t"
+    },
+    ["mysteriouslight"] = {
+        spell = "Mysterious Light",
+        target = "t"
+    },
+    ["coldwave"] = {
+        spell = "Cold Wave",
+        target = "t"
+    },
+    ["poisonbreath"] = {
+        spell = "Poison Breath",
+        target = "t"
+    },
+    ["stinkinggas"] = {
+        spell = "Stinking Gas",
+        target = "t"
+    },
+    ["mementomori"] = {
+        spell = "Memento Mori",
+        target = "me"
+    },
+    ["terrortouch"] = {
+        spell = "Terror Touch",
+        target = "t"
+    },
+    ["spinalcleave"] = {
+        spell = "Spinal Cleave",
+        target = "t"
+    },
+    ["bloodsaber"] = {
+        spell = "Blood Saber",
+        target = "t"
+    },
+    ["digest"] = {
+        spell = "Digest",
+        target = "t"
+    },
+    ["mandibularbite"] = {
+        spell = "Mandibular Bite",
+        target = "t"
+    },
+    ["cursedsphere"] = {
+        spell = "Cursed Sphere",
+        target = "t"
+    },
+    ["sickleslash"] = {
+        spell = "Sickle Slash",
+        target = "t"
+    },
+    ["cocoon"] = {
+        spell = "Cocoon",
+        target = "me"
+    },
+    ["filamentedhold"] = {
+        spell = "Filamented Hold",
+        target = "t"
+    },
+    ["pollen"] = {
+        spell = "Pollen",
+        target = "t"
+    },
+    ["powerattack"] = {
+        spell = "Power Attack",
+        target = "t"
+    },
+    ["deathscissors"] = {
+        spell = "Death Scissors",
+        target = "t"
+    },
+    ["magnetitecloud"] = {
+        spell = "Magnetite Cloud",
+        target = "t"
+    },
+    ["eyesonme"] = {
+        spell = "Eyes on Me",
+        target = "t"
+    },
+    ["freneticrip"] = {
+        spell = "Frenetic Rip",
+        target = "t"
+    },
+    ["frightfulroar"] = {
+        spell = "Frightful Roar",
+        target = "t"
+    },
+    ["hecatombwave"] = {
+        spell = "Hecatomb Wave",
+        target = "t"
+    },
+    ["bodyslam"] = {
+        spell = "Body Slam",
+        target = "t"
+    },
+    ["radiantbreath"] = {
+        spell = "Radiant Breath",
+        target = "t"
+    },
+    ["helldive"] = {
+        spell = "Helldive",
+        target = "t"
+    },
+    ["jetstream"] = {
+        spell = "Jet Stream",
+        target = "t"
+    },
+    ["blooddrain"] = {
+        spell = "Blood Drain",
+        target = "t"
+    },
+    ["soundblast"] = {
+        spell = "Sound Blast",
+        target = "t"
+    },
+    ["feathertickle"] = {
+        spell = "Feather Tickle",
+        target = "t"
+    },
+    ["featherbarrier"] = {
+        spell = "Feather Barrier",
+        target = "me"
+    },
+    ["jettatura"] = {
+        spell = "Jettatura",
+        target = "t"
+    },
+    ["footkick"] = {
+        spell = "Foot Kick",
+        target = "t"
+    },
+    ["wildcarrot"] = {
+        spell = "Wild Carrot",
+        target = "t"
+    },
+    ["voracioustrunk"] = {
+        spell = "Voracious Trunk",
+        target = "t"
+    },
+    ["healingbreeze"] = {
+        spell = "Healing Breeze",
+        target = "me"
+    },
+    ["chaoticeye"] = {
+        spell = "Chaotic Eye",
+        target = "t"
+    },
+    ["ramcharge"] = {
+        spell = "Ram Charge",
+        target = "t"
+    },
+    ["clawcyclone"] = {
+        spell = "Claw Cyclone",
+        target = "t"
+    },
+    ["lowing"] = {
+        spell = "Lowing",
+        target = "t"
+    },
+    ["dimensionaldeath"] = {
+        spell = "Dimensional Death",
+        target = "t"
+    },
+    ["heatbreath"] = {
+        spell = "Heat Breath",
+        target = "t"
+    },
+    ["blankeye"] = {
+        spell = "Blank Gaze",
+        target = "t"
+    },
+    ["magicfruit"] = {
+        spell = "Magic Fruit",
+        target = "t"
+    },
+    ["uppercut"] = {
+        spell = "Uppercut",
+        target = "t"
+    },
+    ["1000needles"] = {
+        spell = "1000 Needles",
+        target = "t"
+    },
+    ["pineconebomb"] = {
+        spell = "Pinecone Bomb",
+        target = "t"
+    },
+    ["sproutsmack"] = {
+        spell = "Sprout Smack",
+        target = "t"
+    },
+    ["soporific"] = {
+        spell = "Soporific",
+        target = "t"
+    },
+    ["queasyshroom"] = {
+        spell = "Queasyshroom",
+        target = "t"
+    },
+    ["wildoats"] = {
+        spell = "Wild Oats",
+        target = "t"
+    },
+    ["badbreath"] = {
+        spell = "Bad Breath",
+        target = "t"
+    },
+    ["geistwall"] = {
+        spell = "Geist Wall",
+        target = "t"
+    },
+    ["awfuleye"] = {
+        spell = "Awful Eye",
+        target = "t"
+    },
+    ["frostbreath"] = {
+        spell = "Frost Breath",
+        target = "t"
+    },
+    ["infrasonics"] = {
+        spell = "Infrasonics",
+        target = "t"
+    },
+    ["disseverment"] = {
+        spell = "Disseverment",
+        target = "t"
+    },
+    ["actinicburst"] = {
+        spell = "Actinic Burst",
+        target = "t"
+    },
+    ["reactorcool"] = {
+        spell = "Reactor Cool",
+        target = "me"
+    },
+    ["salinecoat"] = {
+        spell = "Saline Coat",
+        target = "me"
+    },
+    ["plasmacharge"] = {
+        spell = "Plasma Charge",
+        target = "me"
+    },
+    ["temporalshift"] = {
+        spell = "Temporal Shift",
+        target = "t"
+    },
+    ["verticalcleave"] = {
+        spell = "Vertical Cleave",
+        target = "t"
+    },
+    ["blastbomb"] = {
+        spell = "Blastbomb",
+        target = "t"
+    },
+    ["battledance"] = {
+        spell = "Battle Dance",
+        target = "t"
+    },
+    ["sandspray"] = {
+        spell = "Sandspray",
+        target = "t"
+    },
+    ["grandslam"] = {
+        spell = "Grand Slam",
+        target = "t"
+    },
+    ["headbutt"] = {
+        spell = "Head Butt",
+        target = "t"
+    },
+    ["bombtoss"] = {
+        spell = "Bomb Toss",
+        target = "t"
+    },
+    ["frypan"] = {
+        spell = "Frypan",
+        target = "t"
+    },
+    ["flyinghippress"] = {
+        spell = "Flying Hip Press",
+        target = "t"
+    },
+    ["hydroshot"] = {
+        spell = "Hydro Shot",
+        target = "t"
+    },
+    ["diamondhide"] = {
+        spell = "Diamondhide",
+        target = "me"
+    },
+    ["enervation"] = {
+        spell = "Enervation",
+        target = "t"
+    },
+    ["lightofpenance"] = {
+        spell = "Light of Penance",
+        target = "t"
+    },
+    ["warmup"] = {
+        spell = "Warm-up",
+        target = "me"
+    },
+    ["firespit"] = {
+        spell = "Firespit",
+        target = "t"
+    },
+    ["featherstorm"] = {
+        spell = "Feather Storm",
+        target = "t"
+    },
+    ["tailslap"] = {
+        spell = "Tail Slap",
+        target = "t"
+    },
+    ["hystericbarrage"] = {
+        spell = "Hysteric Barrage",
+        target = "t"
+    },
+    ["amplification"] = {
+        spell = "Amplification",
+        target = "me"
+    },
+    ["cannonball"] = {
+        spell = "Cannonball",
+        target = "t"
+    },
+    ["mindblast"] = {
+        spell = "Mind Blast",
+        target = "t"
+    },
+    ["exuviation"] = {
+        spell = "Exuviation",
+        target = "t"
+    },
+    ['magicHammer'] = {
+        spell = "Magic Hammer",
+        target = "t"
+    },
+    ['zephyrMantle'] = {
+        spell = "Zephyr Mantle",
+        target = "me"
+    },
+    ['regurgitation'] = {
+        spell = "Regurgitation",
+        target = "t"
+    },
+    ['seedspray'] = {
+        spell = "Seedspray",
+        target = "t"
+    },
+    ['corrosiveOoze'] = {
+        spell = "Corrosive Ooze",
+        target = "t"
+    },
+    ['spiralSpin'] = {
+        spell = "Spiral Spin",
+        target = "t"
+    },
+    ['asuranClaws'] = {
+        spell = "Asuran Claws",
+        target = "t"
+    },
+    ['subZeroSmash'] = {
+        spell = "Sub Zero Smash",
+        target = "t"
+    },
+    ['triumphantRoar'] = {
+        spell = "Triumphant Roar",
+        target = "t"
+    },
+    ['acridStream'] = {
+        spell = "Acrid Stream",
+        target = "t"
+    },
+    ['blazingBound'] = {
+        spell = "Blazing Bound",
+        target = "t"
+    },
+    ['pleniluneEmbrace'] = {
+        spell = "Plenilune Embrace",
+        target = "t"
+    },
+    ['demoralizingRoar'] = {
+        spell = "Demoralizing Roar",
+        target = "t"
+    },
+    ['cimicineDischarge'] = {
+        spell = "Cimicine Discharge",
+        target = "t"
+    },
+    ['animatingWail'] = {
+        spell = "Animating Wail",
+        target = "me"
+    },
+    ['batteryCharge'] = {
+        spell = "Battery Charge",
+        target = "me"
+    },
+    ['leafstorm'] = {
+        spell = "Leafstorm",
+        target = "t"
+    },
+    ['regeneration'] = {
+        spell = "Regeneration",
+        target = "t"
+    },
+    ['finalSting'] = {
+        spell = "Final Sting",
+        target = "t"
+    },
+    ['goblinRush'] = {
+        spell = "Goblin Rush",
+        target = "t"
+    },
+    ['vanityDive'] = {
+        spell = "Vanity Dive",
+        target = "t"
+    },
+    ['magicBarrier'] = {
+        spell = "Magic Barrier",
+        target = "me"
+    },
+    ['whirlOfRage'] = {
+        spell = "Whirl Of Rage",
+        target = "t"
+    },
+    ['benthicTyphoon'] = {
+        spell = "Benthic Typhoon",
+        target = "t"
+    },
+    ['auroralDrape'] = {
+        spell = "Auroral Drape",
+        target = "t"
+    },
+    ['osmosis'] = {
+        spell = "Osmosis",
+        target = "t"
+    },
+    ['quadContinuum'] = {
+        spell = "Quad Continuum",
+        target = "t"
+    },
+    ['fantod'] = {
+        spell = "Fantod",
+        target = "me"
+    },
+    ['thermalPulse'] = {
+        spell = "Thermal Pulse",
+        target = "t"
+    },
+    ['emptyThrash'] = {
+        spell = "Empty Thrash",
+        target = "t"
+    },
+    ['dreamFlower'] = {
+        spell = "Dream Flower",
+        target = "t"
+    },
+    ['occultation'] = {
+        spell = "Occultation",
+        target = "me"
+    },
+    ['chargedWhisker'] = {
+        spell = "Charged Whisker",
+        target = "t"
+    },
+    ['windsOfPromy'] = {
+        spell = "Winds Of Promy",
+        target = "t"
+    },
+    ['deltaThrust'] = {
+        spell = "Delta Thrust",
+        target = "t"
+    },
+    ['evryoneGrudge'] = {
+        spell = "Evryone Grudge",
+        target = "t"
+    },
+    ['reavingWind'] = {
+        spell = "Reaving Wind",
+        target = "t"
+    },
+    ['barrierTusk'] = {
+        spell = "Barrier Tusk",
+        target = "me"
+    },
+    ['mortalRay'] = {
+        spell = "Mortal Ray",
+        target = "t"
+    },
+    ['waterBomb'] = {
+        spell = "Water Bomb",
+        target = "t"
+    },
+    ['heavyStrike'] = {
+        spell = "Heavy Strike",
+        target = "t"
+    },
+    ['darkOrb'] = {
+        spell = "Dark Orb",
+        target = "t"
+    },
+    ['whitewind'] = {
+        spell = "White Wind",
+        target = "me"
+    },
+    ['suddenlunge'] = {
+        spell = "Sudden Lunge",
+        target = "t"
+    },
+    ['quadrastrike'] = {
+        spell = "Quadrastrike",
+        target = "t"
+    },
+    ['vaporSpray'] = {
+        spell = "Vapor Spray",
+        target = "t"
+    },
+    ['thunderBreath'] = {
+        spell = "Thunder Breath",
+        target = "t"
+    },
+    ['orcishCounterstance'] = {
+        spell = "Orcish Counterstance",
+        target = "me"
+    },
+    ['amorphicSpikes'] = {
+        spell = "Amorphic Spikes",
+        target = "t"
+    },
+    ['windBreath'] = {
+        spell = "Wind Breath",
+        target = "t"
+    },
+    ['barbedCrescent'] = {
+        spell = "Barbed Crescent",
+        target = "t"
+    },
 }
-
 infoLog("Blue Spell list loaded")
